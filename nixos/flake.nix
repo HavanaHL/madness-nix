afb21ca9914cc0cc2d6fcab5442cce3594829d6a
HavanaHL <deivepython@protonmail.com>
2025-11-05 18:29:17 -0300

    Socorro

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,18 +4,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; 
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, home-manager, chaotic} @ inputs: {
+  outputs = {self, nixpkgs, home-manager} @ inputs: {
 
    nixosConfigurations = {
      "Cheshire" = nixpkgs.lib.nixosSystem {
        specialArgs = {inherit inputs;};
        system = "x86_64-linux";
-       modules = [ ./config.nix chaotic.nixosModules.default ];
+       modules = [ ./config.nix ];
      };
    }; 
 
